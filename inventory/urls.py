2a89d16111726453893a97f894d12f4e356c0a0e
hasanpp <hasanpp02@gmail.com>
2025-03-28 17:08:46 +0530

    Render hosting

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import InventoryManagementView
+from django.http import HttpResponse
+
+
+
+def home(request):
+    return HttpResponse("This is the inventory API")
 
 urlpatterns = [
     path("items/", InventoryManagementView.as_view(), name="item-view"),
     path("items/<int:item_id>/", InventoryManagementView.as_view(), name="item-view-get"),
+    path('', home, name='home'),
 ]
